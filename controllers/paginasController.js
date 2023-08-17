c88f4d0c3d5c4efddf746811654456afd100799b
AntonioValentin <cesar_anto30@hotmail.com>
2023-08-16 21:53:32 -0600

    Configuracion para consultar los datos d ela base de datos

---
+++ controllers/paginasController.js
@@ -0,0 +1,38 @@
+import { Viaje } from '../models/Viaje.js'
+
+const paginaInicio = (req, res) => {
+    res.render('inicio', {
+        pagina: 'Inicio'
+    })
+}
+
+const paginaNosotros = (req, res) => {
+    res.render('nosotros', {
+        pagina: 'Nosotros',
+    })
+}
+
+const paginaViajes = async (req, res) => {
+    // Consultar DB 
+    const viajes =  await Viaje.findAll();
+
+    console.log(viajes);
+
+    res.render('viajes', {
+        pagina: 'Viajes',
+        viajes,
+    })
+}
+
+const paginaTestimoniales = (req, res) => {
+    res.render('testimoniales', {
+        pagina: 'Testimoniales',
+    })
+}
+
+export {
+    paginaInicio,
+    paginaNosotros,
+    paginaViajes,
+    paginaTestimoniales
+}
