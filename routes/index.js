2148a3cde518af4c970395fd5f8ba86684cc209d
AntonioValentin <cesar_anto30@hotmail.com>
2023-08-16 21:54:32 -0600

    Actualizacion de nuevas vistas para el DB

--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,15 @@
 import express from 'express'
+import { paginaInicio, 
+        paginaNosotros,
+        paginaViajes, 
+        paginaTestimoniales }
+ from '../controllers/paginasController.js'
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.render('inicio', {
-        pagina: 'Inicio'
-    })
-})
-router.get('/nosotros', (req, res) => {
-    res.render('nosotros', {
-        pagina: 'Nosotros',
-    })
-})
-router.get('/viajes', (req, res) => {
-    res.render('viajes', {
-        pagina: 'Viajes',
-    })
-})
-router.get('/testimoniales', (req, res) => {
-    res.render('testimoniales', {
-        pagina: 'Testimoniales',
-    })
-})
+router.get('/', paginaInicio)
+router.get('/nosotros', paginaNosotros)
+router.get('/viajes', paginaViajes)
+router.get('/testimoniales', paginaTestimoniales)
 
 export default router
