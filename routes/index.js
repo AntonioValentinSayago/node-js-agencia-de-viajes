e07f11e9bb72524c8cbe35d01611ff572c0dcf45
AntonioValentin <cesar_anto30@hotmail.com>
2023-08-17 15:31:22 -0600

    Nueva configuracion para el envio de información

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,23 @@ import express from 'express'
 import { paginaInicio, 
         paginaNosotros,
         paginaViajes, 
-        paginaTestimoniales }
+        paginaTestimoniales,
+        paginaDetalleViaje }
  from '../controllers/paginasController.js'
+import { guardarTestimonial } from '../controllers/testimonialController.js';
+
+
 
 const router = express.Router();
 
 router.get('/', paginaInicio)
+
 router.get('/nosotros', paginaNosotros)
+
 router.get('/viajes', paginaViajes)
+router.get('/viajes/:slug', paginaDetalleViaje)
+
 router.get('/testimoniales', paginaTestimoniales)
+router.post('/testimoniales', guardarTestimonial)
 
 export default router
